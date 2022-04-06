c2d0223ef6fddb1d5ef9e70fffb98c35bf32f325
galtoubul <galtoubul@gmail.com>
2022-04-06 23:14:44 +0300

    question | showing questions from the external API

--- src/componenets/Question/Question.js
+++ src/componenets/Question/Question.js
@@ -1,11 +1,9 @@
 import "./Question.css";
 
 const Question = (props) => {
-  const questionTxt =
-    "The creeper in Mincraft was the result of a bug while implementing which creature?";
   return (
     <div className="question-text-container">
-      <div className="question-text">{questionTxt}</div>
+      <div className="question-text">{props.questionText}</div>
     </div>
   );
 };
--- src/componenets/Question/QuestionForm.js
+++ src/componenets/Question/QuestionForm.js
@@ -1,15 +1,54 @@
 import "./QuestionForm.css";
 import Question from "./Question.js";
+import Answer from "./Answer.js";
+import axios from "axios";
+import React from "react";
 
-const QuestionForm = (props) => {
-  return (
-    <div className="question-container">
-      <Question />
-      <div className="answers-row-1"></div>
-      <div className="answers-row-2"></div>
-      <div className="question-number-container"></div>
-    </div>
-  );
-};
+class QuestionForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      question: "",
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    console.log("hi");
+    axios
+      .get("https://opentdb.com/api.php?amount=1")
+      .then((response) => {
+        console.log(response.data.results[0]["question"]);
+        this.setState({ question: response.data.results[0]["question"] });
+        console.log(this.state.question[0]);
+      })
+      .catch((error) => console.log(error));
+  }
+
+  renderAnswer(i, props) {
+    console.log(i);
+    return <Answer onClick={this.handleClick} />;
+  }
+
+  render() {
+    return (
+      <div className="question-container">
+        <div className="question-box">
+          <Question questionText={this.state.question} />
+
+          <div className="answers-row-1">
+            {this.renderAnswer(0)}
+            {this.renderAnswer(1)}
+          </div>
+          <div className="answers-row-2">
+            {this.renderAnswer(2)}
+            {this.renderAnswer(3)}
+          </div>
+          <div className="question-number-container"></div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default QuestionForm;
