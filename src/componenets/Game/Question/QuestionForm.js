cc98662823fc9dd4e393dd7099dbc19dbacc8cc2
galtoubul <galtoubul@gmail.com>
2022-04-10 11:33:14 +0300

    cleaning comments, less confetti, uncomment timer

--- src/componenets/Game/Question/QuestionForm.js
+++ src/componenets/Game/Question/QuestionForm.js
@@ -68,7 +68,7 @@ const QuestionForm = (props) => {
       refAnimationInstance.current({
         ...opts,
         origin: { y: 0.7 },
-        particleCount: Math.floor(200 * particleRatio),
+        particleCount: Math.floor(75 * particleRatio),
       });
   }, []);
 
@@ -77,28 +77,6 @@ const QuestionForm = (props) => {
       spread: 50,
       startVelocity: 55,
     });
-
-    // makeShot(0.2, {
-    //   spread: 60,
-    // });
-
-    // makeShot(0.35, {
-    //   spread: 100,
-    //   decay: 0.91,
-    //   scalar: 0.8,
-    // });
-
-    // makeShot(0.1, {
-    //   spread: 120,
-    //   startVelocity: 25,
-    //   decay: 0.92,
-    //   scalar: 1.2,
-    // });
-
-    // makeShot(0.1, {
-    //   spread: 120,
-    //   startVelocity: 45,
-    // });
   }, [makeShot]);
 
   function handleClick(i) {
